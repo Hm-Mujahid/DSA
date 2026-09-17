ec138fef948b7c6f873f2a27ed348eb55c5d42e9
Mujahid pasha <hmmujahid843@gmail.com>
2026-09-17 10:06:44 +0530

    new commit

---
+++ day1.py
@@ -0,0 +1,26 @@
+'''x = input()
+print(x)
+print(type(x))'''
+
+# x2 = input().split()
+# print(x2[0])
+# print(type(x2[0]))
+
+# x2 = int,input().split()
+# print(x2)
+# print(type(x2))
+
+# x2 = map(float,input().split())
+# print(x2)
+# print(type(x2[0]))
+# print(type(x2))
+
+# x2 = list(map(float,input().split()))
+# print(x2)
+# print(type(x2[0]))
+# print(type(x2))
+
+x1,x2,x3 = map(float,input().split())
+print(x2,x1)
+print(type(x3))
+print(type(x2))
